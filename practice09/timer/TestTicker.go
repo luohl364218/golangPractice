2e4ed4517cff6ee60d0670eca934f98c6441e6f1
luohl364218 <luohl364218@gmail.com>
2018-05-23 21:32:57 +0800

    [55]定时器

--- practice09/timer/TestTicker.go
+++ practice09/timer/TestTicker.go
@@ -7,15 +7,21 @@ import (
 
 func main()  {
 	//定时器
-	v := time.NewTicker(time.Second)
+	/*v := time.NewTicker(time.Second)
 
 	for {
 		select {
 		case <-v.C:
 			fmt.Println("time out")
 		}
+	}*/
+
+	//死循环
+	t := time.NewTicker(time.Second)
+	for v := range t.C {
+		fmt.Println("hello, ", v)
 	}
-
-
+	//退出时需要stop 防止内存泄漏
+	t.Stop()
 }
 
