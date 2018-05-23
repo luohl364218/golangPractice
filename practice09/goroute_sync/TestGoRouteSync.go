3ac5b23a0635cf449b2edb2407e920128e8ec07c
luohl364218 <luohl364218@gmail.com>
2018-05-23 20:09:30 +0800

    [51]使用chan记录goroute关闭情况

--- practice09/goroute_sync/TestGoRouteSync.go
+++ practice09/goroute_sync/TestGoRouteSync.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func calc(taskChan , resultChan chan int)  {
+func calc(taskChan , resultChan chan int, exitChan chan bool)  {
 
 	//判断是不是素数
 	for v := range taskChan{
@@ -21,34 +20,44 @@ func calc(taskChan , resultChan chan int)  {
 		}
 
 	}
+	exitChan <- true
+	fmt.Println("exit calc")
 }
 
 func main()  {
 
 	taskChan := make(chan int, 1000)
 	resultChan := make(chan int, 1000)
+	//使用chan
+	exitChan := make(chan bool, 8)
 
 	//新开协程跑匿名函数
 	go func() {
 		for i := 1; i < 10000; i++ {
 			taskChan <- i
 		}
+		//关闭chan之后range taskChan时会在取完后自动退出
 		close(taskChan)
 	}()
 
 	//新开8个协程计算
 	for i := 0; i < 8; i++ {
-		go calc(taskChan, resultChan)
+		go calc(taskChan, resultChan, exitChan)
 	}
 
 	//新开协程取结果
 	go func() {
-		for {
-			v := <- resultChan
-			fmt.Println(v)
+		//等待所有goroute退出
+		for i := 0; i < 8; i++ {
+			//阻塞住，只要取8个值，说明所有goroute都退出，说明结果已经计算完了
+			<- exitChan
+			fmt.Println("wait goroute ", i, " exited")
 		}
+		close(resultChan)
 	}()
 
-	time.Sleep(2 * time.Second)
+	for v := range resultChan{
+		fmt.Println(v)
+	}
 }
 
