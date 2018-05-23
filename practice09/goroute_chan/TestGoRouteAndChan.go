c929b619be4b03f49aebb738ab8f60a792c1ff38
luohl364218 <luohl364218@gmail.com>
2018-05-23 20:27:51 +0800

    [52]改造：goroute之间协同退出

--- practice09/goroute_chan/TestGoRouteAndChan.go
+++ practice09/goroute_chan/TestGoRouteAndChan.go
@@ -25,4 +25,41 @@ func main() {
 	go write(chann)
 	go read(chann)
 	time.Sleep(5 * time.Second)
+
+	fmt.Println("----------------------")
+
+	test()
+}
+
+func test() {
+	ch := make(chan int,10)
+	exitCh := make(chan struct{}, 2)
+	go send(ch, exitCh)
+	go recv(ch, exitCh)
+
+	//等待所有goroute退出【采用计数方法比较稳】
+	for i := 0; i < 2; i++{
+		<- exitCh
+	}
+}
+
+func send(ch chan int, exitCh chan struct{})  {
+	for i := 0; i < 10; i++ {
+		ch <- i
+		fmt.Println("generate ", i)
+	}
+	//记得关闭
+	close(ch)
+	exitCh <- struct{}{}
+}
+
+func recv(ch chan int, exitCh chan struct{}) {
+	for {
+		v, ok := <- ch
+		if !ok {
+			break
+		}
+		fmt.Println(v)
+	}
+	exitCh <- struct{}{}
 }
