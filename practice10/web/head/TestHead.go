7ca23ecaabfaf518131c5848005ecb0980f0a6f9
luohl364218 <luohl364218@gmail.com>
2018-05-24 17:24:51 +0800

    [63]http head自定义请求超时时间

--- practice10/web/head/TestHead.go
+++ practice10/web/head/TestHead.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"time"
 )
 
 var urlStr = []string{
@@ -13,6 +14,12 @@ var urlStr = []string{
 
 func main() {
 
+	//test()
+	testClient()
+
+}
+
+func test() {
 	for _, v := range urlStr {
 		resp, err := http.Head(v)
 		if err != nil {
@@ -21,5 +28,20 @@ func main() {
 		}
 		fmt.Printf("head success, status: %v\n", resp.Status)
 	}
+}
 
+func testClient() {
+	for _, v := range urlStr {
+		//自定义客户端结构体
+		c := http.Client{
+			Timeout:time.Second * 2,
+		}
+
+		resp, err := c.Head(v)
+		if err != nil {
+			fmt.Printf("head %s failed,err:%v\n", v, err)
+			continue
+		}
+		fmt.Printf("head success, status: %v\n", resp.Status)
+	}
 }
