65e218645c117c9a82b6b6fb82328fd93a388c13
luohl364218 <luohl364218@gmail.com>
2018-05-25 15:28:42 +0800

    [68]聊天室开发：服务端-TCP服务模块1.0

--- chat_room/chat_server/main/main.go
+++ chat_room/chat_server/main/main.go
@@ -4,4 +4,6 @@ import "time"
 
 func main() {
 	initRedis("localhost:6379", 16, 1024, 300 * time.Second)
+
+	initServer("0.0.0.0:10000")
 }
---
+++ chat_room/chat_server/main/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net"
+	"fmt"
+)
+
+func initServer(addr string) (err error) {
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("listen err:", err)
+		return
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("accept err:", err)
+			continue
+		}
+		go handleConnect(conn)
+	}
+}
+
+func handleConnect(conn net.Conn)  {
+	defer conn.Close()
+	for {
+		buf := make([]byte, 512)
+		//记录读取的数量，不然会输出很多空白的内容
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read err:", err)
+			return
+		}
+		fmt.Printf(string(buf[:n]))
+		//todo
+	}
+}
